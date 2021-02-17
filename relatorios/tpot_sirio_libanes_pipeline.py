f1c552fdc205eca8415d983d0923c4a3a820cc9e
Clarisse de Almeida e Alvarenga <37255795+ClarisseAlvarenga@users.noreply.github.com>
2021-02-17 18:33:21 -0300

    Create tpot_sirio_libanes_pipeline.py

---
+++ relatorios/tpot_sirio_libanes_pipeline.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=None)
+
+# Average CV score on the training set was: 0.775501253132832
+exported_pipeline = make_pipeline(
+    StandardScaler(),
+    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.1, max_depth=9, max_features=1.0, min_samples_leaf=5, min_samples_split=14, n_estimators=100, subsample=0.45)),
+    VarianceThreshold(threshold=0.05),
+    LinearSVC(C=0.001, dual=True, loss="hinge", penalty="l2", tol=0.001)
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
